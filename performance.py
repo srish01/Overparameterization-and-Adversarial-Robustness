2ce4028bd2666d9f637ffee13ebb7bd1f65d45a3
Srishti Gupta <srishtiunica@Srishtis-MacBook-Air.local>
2023-12-11 16:58:06 +0100

    updated performance.py

--- performance.py
+++ performance.py
@@ -6,15 +6,17 @@ from secml.data import CDataset
 from secml.ml import CClassifier
 from secml.ml.peval.metrics import CMetricAccuracy
 
-from config import PLOT_FOLDER
+from folder import PLOT_FOLDER
 
 
 def performance_score_model(classifier: CClassifier, tr_dataset: CDataset, ts_dataset: CDataset):
     tr_y_pred = classifier.predict(tr_dataset.X)
     ts_y_pred = classifier.predict(ts_dataset.X)
-    tr_error = 1 - CMetricAccuracy().performance_score(tr_dataset.Y, tr_y_pred)
-    ts_error = 1 - CMetricAccuracy().performance_score(ts_dataset.Y, ts_y_pred)
-    return tr_error, ts_error
+    tr_acc = CMetricAccuracy().performance_score(tr_dataset.Y, tr_y_pred)
+    ts_acc = CMetricAccuracy().performance_score(ts_dataset.Y, ts_y_pred)
+    tr_error = 1 - tr_acc
+    ts_error = 1 - ts_acc
+    return tr_acc, ts_acc, tr_error, ts_error
 
 
 def log_mse_loss(clf: CClassifier, data: CDataset):
@@ -33,9 +35,9 @@ def test_models(classifiers, tr_dataset, ts_dataset):
     ts_errors = []
     tr_errors = []
     for c in classifiers:
-        tr, ts = performance_score_model(c, tr_dataset, ts_dataset)
-        ts_errors.append(ts)
-        tr_errors.append(tr)
+        tr_acc, ts_acc, tr_err, ts_err = performance_score_model(c, tr_dataset, ts_dataset)
+        ts_errors.append(ts_err)
+        tr_errors.append(tr_err)
     ts_errors = CArray(ts_errors)
     tr_errors = CArray(tr_errors)
 
@@ -90,9 +92,9 @@ def plot_performance(classifiers,
     plt.plot(x_axis.tondarray(), tr_error.tondarray(), label="tr error", c='r')
     plt.plot(x_axis.tondarray(), ts_error.tondarray(), label="ts error", c='b')
     plt.xlabel(xlabel)
-    plt.ylabel("Accuracy")
+    plt.ylabel("Errors")    # previously "Accuracies" but we are actually plotting errors
     plt.title(title)
     plt.legend()
     if savefig:
-        plt.savefig(str(PLOT_FOLDER / f"{savefig}.pdf"))
+        plt.savefig(str(PLOT_FOLDER / f"{savefig}.jpg"))
     plt.show()
